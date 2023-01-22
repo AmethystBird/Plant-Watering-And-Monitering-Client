4167ce719a23f5ee81ff8f89cf3ab2886a622b9f
Nikita The Wyvern <48954549+AmethystBird@users.noreply.github.com>
2023-01-22 21:59:23 +0000

    Sensor button interaction
    - Buttons highlight when clicked
    - Buttons switch which sensor value is being viewed

---
+++ MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿//using Android.Content.Res;
+
+namespace PlantWateringAndMonitoringSystem;
+
+public partial class MainPage : ContentPage
+{
+    float lightValue;
+	float moistureValue;
+	float temperatureValue;
+	float humidityValue;
+	public MainPage()
+	{
+		InitializeComponent();
+
+		lightValue = 0.0f;
+		moistureValue = 1.0f;
+		temperatureValue = 2.0f;
+		humidityValue = 4.0f;
+    }
+	async void OnSensorButtonClicked(object sender, EventArgs e)
+	{
+        Button button = (Button)sender;
+        if (button.Text == "Light")
+        {
+			ChangeSensorToDisplay(lightValue.ToString(), " lux");
+        }
+        if (button.Text == "Moisture")
+        {
+            ChangeSensorToDisplay(moistureValue.ToString(), "%");
+        }
+        if (button.Text == "Temperature")
+        {
+            ChangeSensorToDisplay(temperatureValue.ToString(), " *C");
+        }
+        if (button.Text == "Humidity")
+        {
+            ChangeSensorToDisplay(humidityValue.ToString(), " <humidityLvl>");
+        }
+    }
+    async void OnButtonPressed(object sender, EventArgs e)
+    {
+        Button button = (Button)sender;
+        button.BorderColor = Colors.White;
+    }
+    async void OnButtonReleased(object sender, EventArgs e)
+    {
+        Button button = (Button)sender;
+        button.BorderColor = Colors.Black;
+    }
+    void ChangeSensorToDisplay(string valueDisplay, string measurementSuffix)
+	{
+		Resources["valueLabel"] = valueDisplay + measurementSuffix;
+    }
+    //override 
+    /*private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}*/
+}
+
