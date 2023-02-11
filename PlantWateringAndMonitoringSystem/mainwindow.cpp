dff1733a663c27147919783784701343782c1aee
Nikita The Wyvern <48954549+AmethystBird@users.noreply.github.com>
2023-02-11 14:25:47 +0000

    Original QT app functionality re-implemented
    - Due to new project being created in last commit in order to facilitate different settings, original code has been re-implemented

--- PlantWateringAndMonitoringSystem/mainwindow.cpp
+++ PlantWateringAndMonitoringSystem/mainwindow.cpp
@@ -9,6 +9,10 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    displayMode = false;
+    ToggleDisplayMode();
+    ui->darkModeCheckBox->setChecked(true);
+
     QLineSeries *series = new QLineSeries();
     series->append(0, 6);
     series->append(2, 4);
@@ -34,3 +38,47 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::ToggleDisplayMode()
+{
+    displayMode = !displayMode;
+    if (displayMode)
+    {
+        setStyleSheet("background-color: #101010");
+        ui->titleLabel->setStyleSheet("color: #f0f0f0");
+        ui->optionsMenuFrame->setVisible(false);
+        ui->optionsMenuFrame->setStyleSheet("background-color: #202020");
+        ui->optionsLabel->setStyleSheet("color: #e0e0e0");
+        ui->networkingLabel->setStyleSheet("color: #e0e0e0");
+        ui->preferencesLabel->setStyleSheet("color: #e0e0e0");
+        ui->disconnectPlantButton->setStyleSheet("color: #e0e0e0");
+        ui->forgetPlantButton->setStyleSheet("color: #e0e0e0");
+        ui->darkModeCheckBox->setStyleSheet("color: #e0e0e0");
+        ui->hideOnCloseCheckBox->setStyleSheet("color: #e0e0e0");
+        ui->sendNotificationsCheckBox->setStyleSheet("color: #e0e0e0");
+    }
+    else
+    {
+        setStyleSheet("background-color: #f0f0f0");
+        ui->titleLabel->setStyleSheet("color: #101010");
+        ui->optionsMenuFrame->setVisible(false);
+        ui->optionsMenuFrame->setStyleSheet("background-color: #e0e0e0");
+        ui->optionsLabel->setStyleSheet("color: #202020");
+        ui->networkingLabel->setStyleSheet("color: #202020");
+        ui->preferencesLabel->setStyleSheet("color: #202020");
+        ui->disconnectPlantButton->setStyleSheet("color: #202020");
+        ui->forgetPlantButton->setStyleSheet("color: #202020");
+        ui->darkModeCheckBox->setStyleSheet("color: #202020");
+        ui->hideOnCloseCheckBox->setStyleSheet("color: #202020");
+        ui->sendNotificationsCheckBox->setStyleSheet("color: #202020");
+    }
+}
+
+void MainWindow::on_optionsButton_released()
+{
+    ui->optionsMenuFrame->setVisible(!ui->optionsMenuFrame->isVisible());
+}
+
+void MainWindow::on_darkModeCheckBox_released()
+{
+    ToggleDisplayMode();
+}
