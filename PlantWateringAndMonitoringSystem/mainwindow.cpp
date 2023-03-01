125b7ad66aea2d992d98d58a39286fb280eb3109
Nikita The Wyvern <48954549+AmethystBird@users.noreply.github.com>
2023-03-01 19:55:11 +0000

    Connects to new IotHub (no IoT Central)
    - Connects to new IoTHub that does not make use of IoT Central
    - Should be able to receive messages, however Azure does not currently send messages, so validity is uncertain
    - Minor layout & stylistic developments to GUI of QT application

--- PlantWateringAndMonitoringSystem/mainwindow.cpp
+++ PlantWateringAndMonitoringSystem/mainwindow.cpp
@@ -30,6 +30,7 @@ MainWindow::MainWindow(QWidget *parent)
     QChartView *chartView = new QChartView(chart);
     chartView->setRenderHint(QPainter::Antialiasing);
 
+    chartView->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     ui->verticalLayout->addWidget(chartView);
 }
 
@@ -45,37 +46,49 @@ void MainWindow::ToggleDisplayMode()
     {
         setStyleSheet("background-color: #101010");
         ui->titleLabel->setStyleSheet("color: #f0f0f0");
-        ui->optionsMenuFrame->setVisible(false);
-        ui->optionsMenuFrame->setStyleSheet("background-color: #202020");
+        //ui->optionsMenuFrame->setVisible(false);
+        //ui->optionsMenuFrame->setStyleSheet("background-color: #202020");
         ui->optionsLabel->setStyleSheet("color: #e0e0e0");
         ui->networkingLabel->setStyleSheet("color: #e0e0e0");
         ui->preferencesLabel->setStyleSheet("color: #e0e0e0");
-        ui->disconnectPlantButton->setStyleSheet("color: #e0e0e0");
-        ui->forgetPlantButton->setStyleSheet("color: #e0e0e0");
+        ui->disconnectPlantButton->setStyleSheet("QPushButton {background-color: #202020; color: #e0e0e0;}");
+        ui->forgetPlantButton->setStyleSheet("QPushButton {background-color: #202020; color: #e0e0e0;}");
         ui->darkModeCheckBox->setStyleSheet("color: #e0e0e0");
         ui->hideOnCloseCheckBox->setStyleSheet("color: #e0e0e0");
         ui->sendNotificationsCheckBox->setStyleSheet("color: #e0e0e0");
+        ui->lightButton->setStyleSheet("QPushButton {background-color: #202020; color: #e0e0e0;}");
+        ui->moistureButton->setStyleSheet("QPushButton {background-color: #202020; color: #e0e0e0;}");
+        ui->humidityButton->setStyleSheet("QPushButton {background-color: #202020; color: #e0e0e0;}");
+        ui->temperatureButton->setStyleSheet("QPushButton {background-color: #202020; color: #e0e0e0;}");
+        ui->optionsButton->setStyleSheet("QToolButton {background-color: #202020; color: #e0e0e0;}");
     }
     else
     {
         setStyleSheet("background-color: #f0f0f0");
         ui->titleLabel->setStyleSheet("color: #101010");
-        ui->optionsMenuFrame->setVisible(false);
-        ui->optionsMenuFrame->setStyleSheet("background-color: #e0e0e0");
+        //ui->optionsMenuFrame->setVisible(false);
+        //ui->optionsMenuFrame->setStyleSheet("background-color: #e0e0e0");
         ui->optionsLabel->setStyleSheet("color: #202020");
         ui->networkingLabel->setStyleSheet("color: #202020");
         ui->preferencesLabel->setStyleSheet("color: #202020");
-        ui->disconnectPlantButton->setStyleSheet("color: #202020");
-        ui->forgetPlantButton->setStyleSheet("color: #202020");
+        ui->disconnectPlantButton->setStyleSheet("QPushButton {background-color: #e0e0e0; color: #202020;}");
+        ui->forgetPlantButton->setStyleSheet("QPushButton {background-color: #e0e0e0; color: #202020;}");
         ui->darkModeCheckBox->setStyleSheet("color: #202020");
         ui->hideOnCloseCheckBox->setStyleSheet("color: #202020");
         ui->sendNotificationsCheckBox->setStyleSheet("color: #202020");
+        ui->lightButton->setStyleSheet("QPushButton {background-color: #e0e0e0; color: #202020;}");
+        ui->moistureButton->setStyleSheet("QPushButton {background-color: #e0e0e0; color: #202020;}");
+        ui->humidityButton->setStyleSheet("QPushButton {background-color: #e0e0e0; color: #202020;}");
+        ui->temperatureButton->setStyleSheet("QPushButton {background-color: #e0e0e0; color: #202020;}");
+        ui->optionsButton->setStyleSheet("QToolButton {background-color: #e0e0e0; color: #202020;}");
     }
 }
 
 void MainWindow::on_optionsButton_released()
 {
-    ui->optionsMenuFrame->setVisible(!ui->optionsMenuFrame->isVisible());
+    //ui->optionsMenuFrame->setVisible(!ui->optionsMenuFrame->isVisible());
+    ui->widget->setVisible(!ui->widget->isVisible());
+    ui->optionsLabel->setVisible(!ui->optionsLabel->isVisible());
 }
 
 void MainWindow::on_darkModeCheckBox_released()
--- PythonAzureProxy/PAP/device.py
+++ PythonAzureProxy/PAP/device.py
@@ -1,50 +1,14 @@
 import time
-import datetime
-from azure.iot.device import IoTHubDeviceClient, MethodResponse
+from azure.iot.device import IoTHubDeviceClient
 
-CONNECTION_STRING = "HostName=iotc-c2dd98d8-fcdb-485c-9f10-9f15250ad05a.azure-devices.net;DeviceId=1ihhjwmthyh;SharedAccessKey=V+r5zstilUDIZVSp9qmO89Mn1l8u9sK4EhRQUZhpHoA="
+#CONNECTION_STRING = "HostName=PlantSensorHub.azure-devices.net;DeviceId=PlantSensorNucleoBoard;SharedAccessKey=4/easOmixo7XFH1QhgPiZLXWKqNThX1ZpA+rGUakszQ=" #Board Primary
+CONNECTION_STRING = "HostName=PlantSensorHub.azure-devices.net;DeviceId=PythonSensorReceiverClient;SharedAccessKey=q/LCQCIaNAgLahucHJ9Qx12g4/dgoCZucBAsBycyin0=" #Receiver Primary
+#CONNECTION_STRING = "HostName=iotc-c2dd98d8-fcdb-485c-9f10-9f15250ad05a.azure-devices.net;DeviceId=1ihhjwmthyh;SharedAccessKey=V+r5zstilUDIZVSp9qmO89Mn1l8u9sK4EhRQUZhpHoA=" #Central
+#CONNECTION_STRING = "HostName=PlantSensorHub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=t0UsaTv1beO9cF9obQXYHO5UrA1ffGaHGR+bz8vd+wo=" #service
+#CONNECTION_STRING = "HostName=PlantSensorHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=C9x3JOrLzFwfMLYTI+BA0uHmSfga/Oh18IoOxgcqJJQ=" #iothubowner
+#CONNECTION_STRING = "HostName=PlantSensorHub.azure-devices.net;SharedAccessKeyName=device;SharedAccessKey=393jckrInx/tLvP0caKgQWEdGQq9q07yTiz71kXIDHo=" #device
 RECEIVED_MESSAGES = 0
-
-def create_client():
-    # Instantiate the client
-    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
-
-    # Define the handler for method requests
-    def method_request_handler(method_request):
-        if method_request.name == "rebootDevice":
-            # Act on the method by rebooting the device
-            print("Rebooting device")
-            time.sleep(20)
-            print("Device rebooted")
-
-            # ...and patching the reported properties
-            current_time = str(datetime.datetime.now())
-            reported_props = {"rebootTime": current_time}
-            client.patch_twin_reported_properties(reported_props)
-            print( "Device twins updated with latest rebootTime")
-
-            # Create a method response indicating the method request was resolved
-            resp_status = 200
-            resp_payload = {"Response": "This is the response from the device"}
-            method_response = MethodResponse(method_request.request_id, resp_status, resp_payload)
-
-        else:
-            # Create a method response indicating the method request was for an unknown method
-            resp_status = 404
-            resp_payload = {"Response": "Unknown method"}
-            method_response = MethodResponse(method_request.request_id, resp_status, resp_payload)
-
-        # Send the method response
-        client.send_method_response(method_response)
-
-    try:
-        # Attach the handler to the client
-        client.on_method_request_received = method_request_handler
-    except:
-        # In the event of failure, clean up
-        client.shutdown()
-
-    return client
+#DeviceId = "PlantSensorNucleoBoard"
 
 def message_handler(message):
     global RECEIVED_MESSAGES
@@ -59,16 +23,20 @@ def message_handler(message):
     print("Total calls received: {}".format(RECEIVED_MESSAGES))
 
 def main():
-    print ("Starting the IoT Hub Python sample...")
-    client = create_client()
+    print ("Starting the Python IoT Hub C2D Messaging device sample...")
 
-    print ("Waiting for commands, press Ctrl-C to exit")
+    # Instantiate the client
+    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+
+    print ("Waiting for C2D messages, press Ctrl-C to exit")
     try:
-        # Wait for program exit
+        # Attach the handler to the client
+        client.on_message_received = message_handler
+
         while True:
             time.sleep(1000)
     except KeyboardInterrupt:
-        print("IoTHubDeviceClient sample stopped")
+        print("IoT Hub C2D Messaging device sample stopped")
     finally:
         # Graceful exit
         print("Shutting down IoT Hub Client")
@@ -76,3 +44,63 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+#IOTHUB_DEVICE_SECURITY_TYPE = "CONNECTION_STRING"
+#EVENT_HUB_CONSUMER_GROUP = "<event_hub_name>"
+
+#def create_client():
+    # Instantiate the client
+#    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+
+    # Define the handler for method requests
+#    def method_request_handler(method_request):
+#        if method_request.name == "rebootDevice":
+            # Act on the method by rebooting the device
+#            print("Rebooting device")
+#            time.sleep(20)
+#            print("Device rebooted")
+
+            # ...and patching the reported properties
+#            current_time = str(datetime.datetime.now())
+#            reported_props = {"rebootTime": current_time}
+#            client.patch_twin_reported_properties(reported_props)
+#            print( "Device twins updated with latest rebootTime")
+
+            # Create a method response indicating the method request was resolved
+#            resp_status = 200
+#            resp_payload = {"Response": "This is the response from the device"}
+#            method_response = MethodResponse(method_request.request_id, resp_status, resp_payload)
+
+#        else:
+            # Create a method response indicating the method request was for an unknown method
+#            resp_status = 404
+#            resp_payload = {"Response": "Unknown method"}
+#            method_response = MethodResponse(method_request.request_id, resp_status, resp_payload)
+
+        # Send the method response
+#        client.send_method_response(method_response)
+
+#    try:
+        # Attach the handler to the client
+#        client.on_method_request_received = method_request_handler
+#    except:
+        # In the event of failure, clean up
+#        client.shutdown()
+
+#    return client
+
+#def main():
+#    print ("Starting the IoT Hub Python sample...")
+#    client = create_client()
+
+#    print ("Waiting for commands, press Ctrl-C to exit")
+#    try:
+        # Wait for program exit
+#        while True:
+#            time.sleep(1000)
+#    except KeyboardInterrupt:
+#        print("IoTHubDeviceClient sample stopped")
+#    finally:
+        # Graceful exit
+#        print("Shutting down IoT Hub Client")
+#        client.shutdown()
